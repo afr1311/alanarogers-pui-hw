2463cf535cffd04298036a4bf425e7a441a68d76
afr1311 <afr1311@gmail.com>
2022-09-17 19:28:18 -0400

    hw 3 update - added pack select

--- solution-hw3/price-computation.js
+++ solution-hw3/price-computation.js
@@ -5,32 +5,65 @@ var glazelist = [
     { glaze: 'Double chocolate', price: 1.5 }
 ];
 
-var quantity = [
+var packlist = [
     { size: '1', pricemultiply: 1 },
     { size: '3', pricemultiply: 3 },
     { size: '6', pricemultiply: 5 },
     { size: '12', pricemultiply: 10 }
 ];
 
-// links to dropdown in HTML
-var select = document.createElement("glazingOptions");
+// links to glazing dropdown in HTML
+var select = document.getElementById('glazingOptions');
 
-
+// creating dropdown menu by looping through glazelist
 for (let i = 0; i < glazelist.length; i++)
 {
-    let glazeselect = glazelist[i].glaze;
-    select.appendChild(glazeselect);
+    let glazeselect = glazelist[i];
 
+    // creating space to put in each of options
+    var options = document.createElement('option');
+
+    // put information into that spot
+    // taking glaze name from glazelist and putting it in options
+    options.textContent = glazeselect.glaze;
+
+    // taking glaze price from glazelist and putting it in options
+    options.value = glazeselect.price;
+
+    // putting info that was just collected into HTML dropdown menu
+    select.append(options);
 }
+
+
+// links to pack size dropdown in HTML
+var select = document.getElementById('packOptions');
+
+// creating dropdown menu by looping through packlist
+for (let i = 0; i < packlist.length; i++)
+{
+    let packselect = packlist[i];
+
+    // creating space to put in each of options
+    var options = document.createElement('option');
+
+    // put information into that spot
+    // taking pack size from packlist and putting it in options
+    options.textContent = packselect.size;
+
+    // taking pack pricemultiply from packlist and putting it in options
+    options.value = packselect.pricemultiply;
+
+    // putting info that was just collected into HTML dropdown menu
+    select.append(options);
+}
+
+
+
 
 function glazingChange(element) {
 
-
-
     // get value of selected glazing option
     const priceChange = element.value;
-
-    
 
 // update price
 
