a33e645d34154c9ce072b995172f36b29ed657c0
afr1311 <afr1311@gmail.com>
2022-11-30 10:27:59 -0500

    Update fp-cases-filter.js

--- fp/fp-cases-filter.js
+++ fp/fp-cases-filter.js
@@ -1,12 +1,16 @@
+// filter function on cases page
 $('.filters-year a, .filters-domain a, .filters-sector a, .all-cases-button a').on('click', function() {
     let filter = $(this).attr('data-filter')
 
+    // hide
     $('.case-all').hide()
 
+    // show
     $(filter).show()
 
+    // remove .selected class
     $('.filters-year a, .filters-domain a, .filters-sector a, .all-cases-button a').removeClass('selected')
 
+    // add selected class when selected
     $(this).addClass('selected')
-
 })
